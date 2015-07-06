6828b5a408df0ae01cb93ce7df4018428b8f65b3
tegon <ltegon93@gmail.com>
2015-07-05 22:58:53 -0300

    Initializing React JS workflow

--- app/scripts/background.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
-
-chrome.browserAction.setBadgeText({text: '\'Allo'});
-
-console.log('\'Allo \'Allo! Event Page for Browser Action');
---
+++ gulpfile.js
@@ -0,0 +1,42 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream'); // Used to stream bundle for further handling
+var browserify = require('browserify');
+var watchify = require('watchify');
+var reactify = require('reactify');
+var concat = require('gulp-concat');
+
+gulp.task('browserify', function() {
+    var bundler = browserify({
+        entries: ['./app/scripts/src/content.js'], // Only need initial file, browserify finds the deps
+        transform: [reactify], // We want to convert JSX to normal javascript
+        debug: true, // Gives us sourcemapping
+        cache: {}, packageCache: {}, fullPaths: true // Requirement of watchify
+    });
+    var watcher  = watchify(bundler);
+
+    return watcher
+    .on('update', function () { // When any files update
+        var updateStart = Date.now();
+        console.log('Updating!');
+        watcher.bundle() // Create new bundle that uses the cache for high performance
+        .pipe(source('content.js'))
+    // This is where you add uglifying etc.
+        .pipe(gulp.dest('./app/scripts/build/'));
+        console.log('Updated!', (Date.now() - updateStart) + 'ms');
+    })
+    .bundle() // Create the initial bundle when starting the task
+    .pipe(source('content.js'))
+    .pipe(gulp.dest('./app/scripts/build/'));
+});
+
+// I added this so that you see how to run two watch tasks
+gulp.task('css', function () {
+    gulp.watch('styles/**/*.css', function () {
+        return gulp.src('styles/**/*.css')
+        .pipe(concat('content.css'))
+        .pipe(gulp.dest('app/styles/build/'));
+    });
+});
+
+// Just running the two tasks
+gulp.task('default', ['browserify', 'css']);
