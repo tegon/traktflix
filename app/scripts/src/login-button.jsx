2cec9ba4c41aaa9d5b801a1a6a6f4d51e8c5e978
tegon <ltegon93@gmail.com>
2015-07-16 15:26:52 -0300

    Added loading while authorizing user

--- app/scripts/src/login-button.jsx
+++ app/scripts/src/login-button.jsx
@@ -4,6 +4,9 @@ var Request = require('./request.js');
 var Utils = require('./utils.js');
 
 module.exports = React.createClass({
+  getInitialState: function() {
+    return { loading: false }
+  },
   getAuthorizeUrl: function() {
     return Settings.authorizeUri + '?client_id=' + Settings.clientId +
       '&redirect_uri=' + Settings.redirectUri + '&response_type=code';
@@ -13,6 +16,8 @@ module.exports = React.createClass({
     return code.split('=')[1];
   },
   handleClick: function(e) {
+    this.setState({ loading: true });
+
     Utils.Oauth.launch({ 'url': this.getAuthorizeUrl(), 'interactive': true },
       function(redirectUrl) {
         var params = {
@@ -36,20 +41,32 @@ module.exports = React.createClass({
     });
   },
   onTokenSuccess: function(response) {
+    this.setState({ loading: false });
     this.props.onTokenSuccess(response);
   },
   onTokenFailed: function(status, response) {
+    this.setState({ loading: false });
     this.props.onTokenFailed(status, response);
   },
   render: function() {
     Utils.Analytics.sendView('Login');
+    var spinnerStyle;
 
-    var className = 'mdl-button mdl-js-button mdl-button--raised' +
-      ' mdl-button--colored mdl-js-ripple-effect login';
+    if (this.state.loading) {
+      spinnerStyle = { display: 'block' };
+    } else {
+      spinnerStyle = { display: 'none' };
+    }
+
     return(
-      <button onClick={this.handleClick} className={className}>
-        Login with Trakt.tv
-      </button>
+      <div className="login-wrapper">
+        <div className="spinner-wrapper" style={spinnerStyle}>
+          <div className="mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active" />
+        </div>
+        <button onClick={this.handleClick} className="mdl-button mdl-js-button mdl-button--raised mdl-button--colored mdl-js-ripple-effect">
+          Login with Trakt.tv
+        </button>
+      </div>
     );
   }
 });
