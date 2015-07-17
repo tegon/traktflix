accfea978c99fe0a142822a8f8f03d9aaf6e1e29
tegon <ltegon93@gmail.com>
2015-07-17 14:37:14 -0300

    More tests

--- __tests__/info-test.js
+++ __tests__/info-test.js
@@ -11,4 +11,13 @@ describe('Info', function() {
     var h4 = TestUtils.findRenderedDOMComponentWithTag(info, 'h4');
     expect(messages).toContain(h4.getDOMNode().textContent);
   });
+
+  it('Has the correct html classes', function() {
+    var messages = ['Info Message', 'About Message'];
+    var info = TestUtils.renderIntoDocument(<Info messages={messages} />);
+    var card = TestUtils.findRenderedDOMComponentWithClass(info, 'mdl-card mdl-shadow--2dp info-card');
+    var cardTitle = TestUtils.findRenderedDOMComponentWithClass(info, 'mdl-card__title mdl-card--expand');
+    expect(card.getDOMNode().className).toBe('mdl-card mdl-shadow--2dp info-card');
+    expect(cardTitle.getDOMNode().className).toBe('mdl-card__title mdl-card--expand');
+  });
 });
