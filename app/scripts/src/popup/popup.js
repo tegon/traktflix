4cf7e5dd9a3bdb8b25809abac8b8f7623b605d15
tegon <ltegon93@gmail.com>
2015-07-29 23:02:40 -0300

    fix Netflix typo

--- app/scripts/src/popup/popup.js
+++ app/scripts/src/popup/popup.js
@@ -5,10 +5,10 @@ var App = require('./components/app.js');
 
 var notWatchingMessages = [
   'You\'re not watching anything right now :/',
-  'Dude, just open Neflix, plz',
-  'Grab a pillow, a blanket and a movie on Neflix',
-  'Two of five doctors says you should be on Neflix right now',
-  'Get yourself some rest, and some Neflix'
+  'Dude, just open Netflix, plz',
+  'Grab a pillow, a blanket and a movie on Netflix',
+  'Two of five doctors says you should be on Netflix right now',
+  'Get yourself some rest, and some Netflix'
 ];
 
 var aboutMessages = [
