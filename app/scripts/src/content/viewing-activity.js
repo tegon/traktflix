58e2b678e6624c53c337f1b3bf75db7fe00dacc6
tegon <ltegon93@gmail.com>
2015-10-15 20:58:43 -0300

    Added ViewingActivity tests

--- app/scripts/src/content/viewing-activity.js
+++ app/scripts/src/content/viewing-activity.js
@@ -5,12 +5,13 @@ var Request = require('../request.js');
 function ViewingActivity(options) {
   this.item = options.item;
   this.date = options.date;
+  this.url = 'https://www.netflix.com/WiViewingActivity';
 }
 
 ViewingActivity.list = function(options) {
   Request.send({
     method: 'GET',
-    url: 'https://www.netflix.com/WiViewingActivity',
+    url: this.url,
     success: function(response) {
       options.success.call(this, response);
     },
