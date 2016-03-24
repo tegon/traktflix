a1e620eb04a29720947a63e54fdf0adaeeecd43e
Leonardo Tegon <ltegon93@gmail.com>
2016-03-23 23:04:03 -0300

    Removes onPause and replaces setInterval with setTimeout

--- app/scripts/src/content/watch-events.js
+++ app/scripts/src/content/watch-events.js
@@ -1,30 +1,20 @@
 'use strict';
 
-var KEY_SPACE = 32;
-var KEY_ENTER = 13;
-var KEY_LEFT_ARROW = 37;
-var KEY_RIGHT_ARROW = 39;
-
 function WatchEvents(options) {
   this.document = document;
   this.onPlay = options.onPlay;
-  this.onPause = options.onPause;
   this.onStop = options.onStop;
   this.url = location.href;
 }
 
 WatchEvents.prototype = {
   startListeners: function() {
-    this.addClickListener();
     this.addStopListener();
-    this.addKeyUpListener();
     this.addUrlChangeListener();
   },
 
   stopListeners: function() {
-    this.removeClickListener();
     this.removeStopListener();
-    this.removeKeyUpListener();
     this.removeUrlChangeListener();
   },
 
@@ -33,39 +23,21 @@ WatchEvents.prototype = {
   },
 
   addStopListener: function() {
-    window.onbeforeunload = function() {
+    window.onbeforeunload = window.onunload = function() {
       this.onStop();
       this.stopListeners();
     }.bind(this)
   },
 
-  addKeyUpListener: function() {
-    this.document.addEventListener('keyup', this.onKeyUp.bind(this), false);
-  },
-
   addUrlChangeListener: function() {
-    this.urlChangeInterval = setInterval(function() {
+    this.urlChangeInterval = setTimeout(function() {
       if (this.url !== location.href) {
         this.onUrlChange(this.url, location.href);
         this.url = location.href;
       }
-    }.bind(this), 1000);
-  },
-
-  onClick: function(e) {
-    if (e.target.classList.contains('player-play-pause')) {
-      if (e.target.classList.contains('play')) {
-        this.onPlay(e);
-      } else if (e.target.classList.contains('pause')) {
-        this.onPause(e);
-      }
-    } else if (e.target.className === 'player-scrubber-target' ||
-        e.target.className === 'player-scrubber-progress-completed' ||
-        e.target.className === 'player-scrubber-progress-buffered' ||
-        e.target.className === 'player-scrubber-progress' ||
-        e.target.className === 'player-scrubber horizontal') {
-      this.isPlaying() ? this.onPlay(e) : this.onPause(e);
-    }
+      clearTimeout(this.urlChangeInterval);
+      this.addUrlChangeListener();
+    }.bind(this), 500);
   },
 
   onUrlChange: function(oldUrl, newUrl) {
@@ -79,48 +51,14 @@ WatchEvents.prototype = {
     }
   },
 
-  onKeyUp: function(e) {
-    switch (e.which) {
-      /* If the video is playing, the obvious would be call onPause,
-        if isn't playing, call onPlay.
-        But the HTML of the player gets updated before this function is called,
-        this way the correct approach is invert the conditions */
-      case KEY_SPACE:
-        this.isPlaying() ? this.onPlay(e) : this.onPause(e);
-        break;
-      case KEY_ENTER:
-        this.isPlaying() ? this.onPlay(e) : this.onPause(e);
-        break;
-      case KEY_LEFT_ARROW:
-        this.onPause(e);
-        break;
-      case KEY_RIGHT_ARROW:
-        this.onPause(e);
-        break;
-    }
-  },
-
-  removeClickListener: function() {
-    this.document.removeEventListener('click', this.onClick.bind(this), false);
-  },
-
   removeStopListener: function() {
     window.onpopstate = null;
     window.onbeforeunload = null;
   },
 
-  removeKeyUpListener: function() {
-    this.document.removeEventListener('keyup', this.onKeyUp.bind(this), false);
-  },
-
   removeUrlChangeListener: function() {
     clearInterval(this.urlChangeInterval);
   },
-
-  isPlaying: function() {
-    var playPause = this.document.querySelector('.player-play-pause');
-    return playPause && playPause.classList.contains('pause');
-  }
 };
 
 module.exports = WatchEvents;
