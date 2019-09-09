069e116b813ce26956e20f071a258406a01ce163
rafaelgssa <rafael.gssa@gmail.com>
2019-09-09 00:00:19 -0300

    Build changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,19 @@
-const fs = require(`fs`);
-const gulp = require(`gulp`);
-const zip = require(`gulp-zip`);
-const generateManifest = require(`./scripts/generateManifest.js`);
 
-gulp.task(`zip-chrome`, () => {
-  return zipExtension(`chrome`);
-});
-
-gulp.task(`zip-firefox`, () => {
-  return zipExtension(`firefox`);
-});
+const gulp = require('gulp');
+const zip = require('gulp-zip');
 
 function zipExtension(browserName) {
-  fs.writeFileSync(`./build/manifest.json`, generateManifest(browserName));
-
-  return gulp.src([
-    `build/**`
-  ])
-    .pipe(zip(`extension-${browserName}.zip`))
-    .pipe(gulp.dest(`./`));
+  return gulp.src([`build/${browserName}/**`])
+    .pipe(zip(`${browserName}.zip`))
+    .pipe(gulp.dest('./dist'));
 }
 
-gulp.task(`zip-src`, () => {
-  return gulp.src([
-    `README.md`,
-    `src/**`
-  ])
-    .pipe(zip(`src.zip`))
-    .pipe(gulp.dest(`./`));
+gulp.task('zip-chrome', () => {
+  return zipExtension('chrome');
 });
+
+gulp.task('zip-firefox', () => {
+  return zipExtension('firefox');
+});
+
+gulp.task('zip', gulp.parallel('zip-chrome', 'zip-firefox'));
